4efc746087bef716929f3c314527e014ba82c534
Ryan Mearns <ryan.j.mearns@gmail.com>
2022-10-30 23:01:49 +1300

    Add icon buttons

--- apps/storybook/stories/Button.stories.tsx
+++ apps/storybook/stories/Button.stories.tsx
@@ -1,6 +1,5 @@
-import { TooltipProvider } from "@radix-ui/react-tooltip";
 import { ComponentMeta, ComponentStory } from "@storybook/react";
-import { Trash2 } from "react-feather";
+import { EnvelopeIcon } from "@heroicons/react/20/solid";
 
 import { Button } from "ui";
 
@@ -8,53 +7,44 @@ export default {
   title: "Button",
   component: Button,
   argTypes: {
-    color: {
-      options: ["primary", "secondary", "minimal", "destructive"],
+    variant: {
+      options: ["primary", "secondary", "outline"],
       control: { type: "select" },
     },
-    disabled: {
-      options: [true, false],
-      control: { type: "boolean" },
-    },
-    loading: {
-      options: [true, false],
-      control: { type: "boolean" },
-    },
+    // loading: {
+    //   options: [true, false],
+    //   control: { type: "boolean" },
+    // },
     size: {
-      options: ["base", "lg", "icon"],
+      options: ["xs", "sm", "md", "lg", "xl"],
       control: { type: "radio" },
     },
   },
 } as ComponentMeta<typeof Button>;
 
-const Template: ComponentStory<typeof Button> = (args) => <Button />;
-
-export const All = () => (
-  <div>
-    <Button />
-  </div>
+const Template: ComponentStory<typeof Button> = (args: any) => (
+  <Button {...args} />
 );
 
-export const Default = Template.bind({});
-Default.args = {
-  color: "primary",
+export const Simple = Template.bind({});
+Simple.args = {
+  variant: "primary",
   children: "Button Text",
+  size: "xs",
 };
 
-export const Disabled = Template.bind({});
-Disabled.args = {
-  ...Default.args,
-  disabled: true,
-};
-
-export const Loading = Template.bind({});
-Loading.args = {
-  ...Default.args,
-  loading: true,
-};
-export const Icon = Template.bind({});
-Icon.args = {
+export const IconLeading = Template.bind({});
+IconLeading.args = {
   color: "secondary",
-  StartIcon: Trash2,
-  size: "icon",
+  IconLeading: EnvelopeIcon,
+  children: "Button Text",
+  size: "xs",
+};
+
+export const IconTrailing = Template.bind({});
+IconTrailing.args = {
+  color: "secondary",
+  IconTrailing: EnvelopeIcon,
+  children: "Button Text",
+  size: "xs",
 };
--- packages/ui/src/Button.tsx
+++ packages/ui/src/Button.tsx
@@ -1,11 +1,104 @@
-export const Button = () => {
+import { classNames } from "./utils";
+import * as React from "react";
+
+type SVGComponent = React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+
+type ButtonVariant = "primary" | "secondary" | "outline";
+type ButtonIconLeading = SVGComponent;
+type ButtonIconTrailing = SVGComponent;
+type ButtonSize = "xs" | "sm" | "md" | "lg" | "xl";
+
+export type ButtonProps = {
+  variant: ButtonVariant;
+  IconLeading?: ButtonIconLeading;
+  IconTrailing?: ButtonIconTrailing;
+  size: ButtonSize;
+  children: React.ReactNode;
+};
+
+interface buttonClassesProps extends ButtonProps {
+  icon: boolean;
+}
+
+export function buttonClasses({ variant, size, icon }: buttonClassesProps) {
+  return classNames(
+    "inline-flex items-center rounded shadow-sm border",
+
+    // Variant
+    variant === "primary" &&
+      "bg-indigo-600 text-white border-transparent hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2",
+    variant === "secondary" &&
+      "bg-indigo-100 text-indigo-700 border-transparent hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2",
+    variant === "outline" &&
+      "bg-transparent text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2",
+
+    // Size
+    size === "xs" && !icon && "px-2.5 py-1.5 text-xs",
+    size === "sm" && !icon && "px-3 py-2 text-sm",
+    size === "md" && !icon && "px-4 py-2 text-sm",
+    size === "lg" && !icon && "px-4 py-2 text-base",
+    size === "xl" && !icon && "px-6 py-3 text-base",
+
+    size === "xs" && icon && "px-3 py-2 text-xs",
+    size === "sm" && icon && "px-4 py-2 text-sm",
+    size === "md" && icon && "px-4 py-2 text-sm",
+    size === "lg" && icon && "px-4 py-2 text-base",
+    size === "xl" && icon && "px-6 py-3 text-base"
+  );
+}
+
+export function iconClasses({
+  size,
+  leading,
+}: {
+  size: ButtonSize;
+  leading: boolean;
+}) {
+  return classNames(
+    // Size
+    size === "xs" && leading && "-ml-0.5 mr-2 h-4 w-4",
+    size === "sm" && leading && "-ml-1 mr-2 h-5 w-5",
+    size === "md" && leading && "-ml-1 mr-3 h-5 w-5",
+    size === "lg" && leading && "-ml-1 mr-3 h-5 w-5",
+    size === "xl" && leading && "-ml-1 mr-3 h-6 w-6",
+
+    size === "xs" && !leading && "ml-2 -mr-0.5 h-4 w-4",
+    size === "sm" && !leading && "ml-2 -mr-1 h-5 w-5",
+    size === "md" && !leading && "ml-3 -mr-1 h-5 w-5",
+    size === "lg" && !leading && "ml-3 -mr-1 h-5 w-5",
+    size === "xl" && !leading && "ml-3 -mr-1 h-6 w-6"
+  );
+}
+
+export const Button = ({
+  variant = "primary",
+  IconLeading,
+  IconTrailing,
+  children,
+  size = "xs",
+}: ButtonProps) => {
   return (
-    <div className="rounded-md ">
-      <a href="https://turborepo.org/docs/getting-started">
-        <div className="flex w-full items-center justify-center rounded-md border border-transparent bg-black px-8 py-3 text-base font-medium text-white no-underline hover:bg-gray-700 dark:bg-white dark:text-black dark:hover:bg-gray-300 md:py-3 md:px-10 md:text-lg md:leading-6">
-          Read me now
-        </div>
-      </a>
-    </div>
+    <button
+      className={buttonClasses({
+        variant,
+        children,
+        size,
+        icon: IconLeading || IconTrailing ? true : false,
+      })}
+    >
+      {IconLeading && (
+        <IconLeading
+          className={iconClasses({ size, leading: true })}
+          aria-hidden="true"
+        />
+      )}
+      {children}
+      {IconTrailing && (
+        <IconTrailing
+          className={iconClasses({ size, leading: false })}
+          aria-hidden="true"
+        />
+      )}
+    </button>
   );
 };
